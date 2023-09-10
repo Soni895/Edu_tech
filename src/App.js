335742c1424c98bca7b8070d845f266c9ca21def
soni895 <darshansoni895@gmail.com>
2023-09-10 19:42:49 +0530

    today commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <Routes>
-   <Route path="/" element={<Home></Home>}></Route>
+       <Route path="/" element={<Home></Home>}></Route>
 
       </Routes>
     </div>
