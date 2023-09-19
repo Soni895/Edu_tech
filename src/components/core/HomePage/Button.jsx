7d5f41b4fc71b180a4ab57b5deadc5658237ea20
soni895 <darshansoni895@gmail.com>
2023-09-19 19:32:47 +0530

    code updated

--- src/components/core/HomePage/Button.jsx
+++ src/components/core/HomePage/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import {Link, NavLink} from "react-router-dom"
 
 const Button = ({children, active, linkto}) => {
   return (
-    <Link to={linkto}>
+    <NavLink to={linkto}>
 
         <div className={`text-center text-[13px] px-6 py-3 rounded-md font-bold
         ${active ? "bg-yellow-50 text-black":" bg-richblack-800"}
@@ -12,7 +12,7 @@ const Button = ({children, active, linkto}) => {
             {children}
         </div>
 
-    </Link>
+    </NavLink>
   )
 }
 
