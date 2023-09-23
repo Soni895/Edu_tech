6edbcbfef9acb21e48b067c946139825846556f2
soni895 <darshansoni895@gmail.com>
2023-09-23 15:11:02 +0530

    nav bar added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Signup from "./Pages/Signup";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+    <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
       <Routes>
        <Route path="/" element={<Home/>}></Route>
-       <Route path="/signup" element={<Signup/>}></Route>
+       <Route path="/login" element = {<Login  setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/signup" element={<Signup  setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/dashboard" element ={
+          <PrivateRoute isLoggedIn={isLoggedIn}>
+              <Dashboard/>
+          </PrivateRoute>
+       
+        } />
+      
       </Routes>
     </div>
   );
