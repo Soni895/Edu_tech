e03c507cec14865b6119257b4a705a9f60f1a501
soni895 <darshansoni895@gmail.com>
2023-09-26 11:01:23 +0530

    nav bar updated

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -9,71 +9,74 @@ const Navbar = (props) => {
     let setIsLoggedIn = props.setIsLoggedIn;
 
   return (
-    <div className='flex justify-between items-center w-11/12 max-w-[1160px] py-4 mx-auto'>
+    <div className=' border-b-2  border-richblack-800'>
+ <div className='flex justify-between items-center w-11/12 max-w-[1160px] py-4 mx-auto'>
 
-        <Link to="/"> 
-            <img src={logo} alt="Logo" width={160} height={32} loading="lazy"/>
+<Link to="/"> 
+    <img src={logo} alt="Logo" width={160} height={32} loading="lazy"/>
+</Link>
+
+<nav>
+    <ul className='text-richblack-100 flex gap-x-6'>
+        <li>
+            <Link to="/">Home</Link>
+        </li>
+        <li>
+            <Link to="/about">About</Link>
+        </li>
+        <li>
+            <Link to="/contact">Contact</Link>
+        </li>
+    </ul>
+</nav>
+
+{/* Login - SignUp - LogOut - Dashboard */}
+<div className='flex items-center gap-x-4'>
+    { !isLoggedIn &&
+       ( <Link to="/login">
+            <button className='bg-richblack-800 text-richblack-100 py-[8px] 
+            px-[12px] rounded-[8px] border border-richblack-700'>
+                Log in
+            </button>
+        </Link>)
+    }
+    { !isLoggedIn &&
+        (<Link to="/signup">
+            <button  className='bg-richblack-800 text-richblack-100 py-[8px] 
+            px-[12px] rounded-[8px] border border-richblack-700'>
+                Sign up
+            </button>
+        </Link>)
+    }
+    { isLoggedIn &&
+        <Link to="/">
+            <button onClick={() => {
+                setIsLoggedIn(false);
+                toast.success("Logged Out");
+            }}
+            className='bg-richblack-800 text-richblack-100 py-[8px] 
+            px-[12px] rounded-[8px] border border-richblack-700'>
+                Log Out
+            </button>
         </Link>
-
-        <nav>
-            <ul className='text-richblack-100 flex gap-x-6'>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
-
-        {/* Login - SignUp - LogOut - Dashboard */}
-        <div className='flex items-center gap-x-4'>
-            { !isLoggedIn &&
-               ( <Link to="/login">
-                    <button className='bg-richblack-800 text-richblack-100 py-[8px] 
-                    px-[12px] rounded-[8px] border border-richblack-700'>
-                        Log in
-                    </button>
-                </Link>)
-            }
-            { !isLoggedIn &&
-                (<Link to="/signup">
-                    <button  className='bg-richblack-800 text-richblack-100 py-[8px] 
-                    px-[12px] rounded-[8px] border border-richblack-700'>
-                        Sign up
-                    </button>
-                </Link>)
-            }
-            { isLoggedIn &&
-                <Link to="/">
-                    <button onClick={() => {
-                        setIsLoggedIn(false);
-                        toast.success("Logged Out");
-                    }}
-                    className='bg-richblack-800 text-richblack-100 py-[8px] 
-                    px-[12px] rounded-[8px] border border-richblack-700'>
-                        Log Out
-                    </button>
-                </Link>
-            }
-            { isLoggedIn &&
-                <Link to="/dashboard">
-                    <button onClick={()=>
-                    {
-                        setIsLoggedIn(true);
-                        toast.success("Dashboard");
-                    }}
-                     className='bg-richblack-800 text-richblack-100 py-[8px] 
-                    px-[12px] rounded-[8px] border border-richblack-700'>
-                        Dashboard
-                    </button>
-                </Link>
-            }
-        </div>
+    }
+    { isLoggedIn &&
+        <Link to="/dashboard">
+            <button onClick={()=>
+            {
+                setIsLoggedIn(true);
+                toast.success("Dashboard");
+            }}
+             className='bg-richblack-800 text-richblack-100 py-[8px] 
+            px-[12px] rounded-[8px] border border-richblack-700'>
+                Dashboard
+            </button>
+        </Link>
+    }
+</div>
+</div>
     </div>
+   
   )
 }
 
