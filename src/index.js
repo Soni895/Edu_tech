d34fc5990d84bef08ac692dbe65c8d62295b9d25
soni895 <darshansoni895@gmail.com>
2023-09-27 17:21:36 +0530

    code updated

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+
+const store=configureStore(
+    {
+        reducer:Rootreducer,
+    }
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- <BrowserRouter>
+<Provider store={store}>
+<BrowserRouter>
   <App/>
   <Toaster/>
  </BrowserRouter>
+</Provider>
 );
