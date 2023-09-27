251febdf3c9c829456a156759b2c49b2bb7480f8
soni895 <darshansoni895@gmail.com>
2023-09-27 19:04:53 +0530

    item slice updated

--- src/Slices/CartSlice.js
+++ src/Slices/CartSlice.js
@@ -1,1 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-hot-toast';
+import { json } from 'react-router-dom';
+
+const  initialState={
+   totalItems:localStorage.getItem("totalItems")?json.parse(localStorage.getItem("totalItems")):0
+};
+const CartSlice=createSlice(
+    {
+        name:"Cart",
+        initialState,
+        reducers: {
+           setTotalItems(state,value)
+           {
+            state.user=value.payload;
+           }
+
+
+        //    add to cart
+        //    remove to cart
+        //    reset cart
+        
+          },
+
+    }
+)
+export const {setTotalItems}=CartSlice.actions;
+export default CartSlice.reducer;
