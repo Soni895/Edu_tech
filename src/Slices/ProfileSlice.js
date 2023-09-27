f15eb18361dbf9c7b1c63d6dbcba897c0133a1fd
soni895 <darshansoni895@gmail.com>
2023-09-27 18:51:18 +0530

    slices created

---
+++ src/Slices/ProfileSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const  initialState={
+    user:null,
+}
+const profileSlice=createSlice(
+    {
+        name:"Profile",
+        initialState,
+        reducers: {
+           setUser(state,value)
+           {
+            state.user=value.payload;
+           }
+          },
+
+    }
+)
+export const {setUser}=AuthSlice.actions;
+export default profileSlice.reducer;
