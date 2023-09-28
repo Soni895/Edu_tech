b6288b8bfd322272e1e0238b70d5fe3db6ba8692
soni895 <darshansoni895@gmail.com>
2023-09-28 18:44:48 +0530

    api connector added

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,1 +1,17 @@
-import { Axios } from "axios"; 
+import axios, { Axios } from "axios"; 
+
+export const AxiosInstance= axios.create({});
+
+export const ApiConnector=(method,url,bodydata,header,params)=>
+{
+    return AxiosInstance(
+        {
+            method:method,
+            url:url,
+            data:bodydata?bodydata:null,
+            header:header?header:null,
+            params:params?params:null,
+        }
+    )
+
+}
