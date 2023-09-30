9bfd02a1ecfc185e30cbbe716b419708c80d9eca
soni895 <darshansoni895@gmail.com>
2023-09-30 22:32:45 +0530

    api url added

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,1 +1,9 @@
-const Base_Url=process.env.Reac
+const Base_Url=process.env.React_App_Url
+
+console.log("Base_Url=>",Base_Url);
+
+export const Categories= 
+{
+  Category_Api: Base_Url+"/Course/GetAllCategory",
+
+}
