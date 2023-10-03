7b719ba0c88d2ed88212d2295410bb2a612a7432
soni895 <darshansoni895@gmail.com>
2023-10-03 19:37:19 +0530

    base url updated

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,10 +1,10 @@
-// const Base_Url=process.env.Base_Url
-const Base_Url="http://localhost:4000/api/v1/";
+// const REACT_APP_Base_Url=process.env.Base_Url
+const REACT_APP_Base_Url="http://localhost:4000/api/v1/";
 
-console.log("Base_Url=>",Base_Url);
+console.log("REACT_APP_Base_Url=>",REACT_APP_Base_Url);
 
 export const Categories= 
 {
-  Category_Api: Base_Url+"/Course/GetAllCategory",
+  Category_Api: REACT_APP_Base_Url+"/Course/GetAllCategory",
 
 }
