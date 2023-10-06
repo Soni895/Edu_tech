b21c2211789db80acc09cd07cac020b733f251f2
soni895 <darshansoni895@gmail.com>
2023-10-06 18:24:04 +0530

    error page completed

---
+++ src/Pages/Error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div className=' flex items-center justify-center text-3xl  text-white'>Error 404 not found</div>
+  )
+}
+
+export default Error
