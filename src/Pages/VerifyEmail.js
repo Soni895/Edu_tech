9d15cc66d7c6baffa7e7172fb2dd9dfcffbfa337
soni895 <darshansoni895@gmail.com>
2023-10-12 18:41:41 +0530

    handleonsubmit added

--- src/Pages/VerifyEmail.js
+++ src/Pages/VerifyEmail.js
@@ -7,6 +7,13 @@ const VerifyEmail = () => {
   const { loading } = useSelector((state) => state.Auth);
   const dispatch=useDispatch();
   const [otp,setotp]=useState("");
+
+  const handleonsubmit= (event)=>
+  {
+    event.preventDefault();
+    console.log("otp=>", otp);
+
+  }
   return (
     <div className="text-white">
       {loading ? (
@@ -15,7 +22,7 @@ const VerifyEmail = () => {
         <div>
           <h1> verify Email</h1>
           <p>A verification code has been sent to you. Enter the code below</p>
-          <form onSubmit>
+          <form  onSubmit={handleonsubmit}>
           <div>
           <OTPInput
           value={otp}
@@ -29,11 +36,12 @@ const VerifyEmail = () => {
           ></OTPInput>
             </div>
             <button>verify Email</button>
-            <div>
+         
+          </form>
+          <div>
               <Link to={"/login"}> Back to Login</Link>
               <div>resend it</div>
             </div>
-          </form>
         </div>
       )}
     </div>
