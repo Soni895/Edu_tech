26f87240c224a0e432a6a3d3e2b82216a2ed19e5
soni895 <darshansoni895@gmail.com>
2023-10-13 21:31:11 +0530

    conatct us form created

--- src/components/core/Aboutpage/ContactFormSection.js
+++ src/components/core/Aboutpage/ContactFormSection.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Contactusform from "./Contactus";
 export default function ContactFormSection() {
   return (
     
-    <div>ContactFormSection</div>
+    <div className=' mx-auto'>
+<h1> Get in Touch</h1>
+<p>We'd Love here for you ,Please fill out this from</p>
+<Contactusform></Contactusform>
+    </div>
   )
 }
