61e1616cb8e80eac7b87194125a11ea4e7dc9096
soni895 <darshansoni895@gmail.com>
2023-10-13 21:40:23 +0530

    use form import

--- src/components/core/Aboutpage/Contactus.js
+++ src/components/core/Aboutpage/Contactus.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 function   Contactus() {
     const [laoding,setloading]=useState(false);
