1eba43e1d842ad27cb67b93bd1685ae2f5da192a
soni895 <darshansoni895@gmail.com>
2023-10-13 23:27:44 +0530

    form mostly completed

--- src/components/core/Aboutpage/Contactus.js
+++ src/components/core/Aboutpage/Contactus.js
@@ -30,10 +30,67 @@ function   Contactus() {
  },[isSubmitSuccessful,reset])
      
 
-  return (
-    <div>
+const submithandler=async(data)=>
+{
 
+}
+
+  return (
+   <div >
+    <form onSubmit={handleSubmit(submithandler)}>
+    <div>
+    <label htmlFor='firstname'> First Name</label>
+    <input
+        type="text"
+        name="firstname"
+        id="firstname"
+        placeholder='Enter Your First Name'
+         {...register("firstanme",{required:true})}
+    />
+    {
+        errors.firstname&&(
+            <span> please enter your name</span>
+        )
+    }
+      
+      <label htmlFor='lastname'> last Name</label>
+    <input
+        type="text"
+        name="lastname"
+        id="lastname"
+        placeholder='Enter Your Last Name'
+        {...register("lastname")}
+    />
     </div>
+    <div>
+        <labl htmlFor="email"> Email Address</labl>
+        <input
+        type='email'
+        name='email'
+        id='email'
+        placeholder='Enter Email Address'
+        {...register("email",{required:true})}
+        ></input>
+
+        {
+            errors.email &&(
+                <span> please enter valid email</span>
+            )
+        }
+    </div>
+    <div>
+    <label htmlFor='message'> message</label>
+        <textarea
+        name='message'
+        id='message'
+        placeholder='enter message'
+        {...register("message",{required:true})}
+        ></textarea>
+    </div>
+
+  
+    </form>
+   </div>
   )
 }
 
