d4d9c7384274d19073a2c949ba0f1c650f852270
soni895 <darshansoni895@gmail.com>
2023-10-19 19:21:26 +0530

    code update

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -5,8 +5,22 @@ const Dashboard = () => {
 
    const {laoding:authloading}=useSelector((state)=>state.Auth);
    const  {laoding:profilelaoding}=useSelector((state)=>state.profile);
+   if(authloading||profilelaoding){
+    return(
+      <div>
+        laoding...
+      </div>
+    )
+   }
+   
   return (
+
     <div className='flex justify-center items-center text-white text-3xl h-full'>
+  
+   <Sidebar></Sidebar>
+   <></>
+
+
 
     </div>
   )
