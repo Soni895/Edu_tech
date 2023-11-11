7e495c2c7fb03c4cfe1db4ead5e5516a1321b020
soni895 <darshansoni895@gmail.com>
2023-11-11 14:35:58 +0530

    reducer added

--- src/Reducer/index.js
+++ src/Reducer/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import  AuthSlice from "../Slices/AuthSlice";
 import CartSlice from '../Slices/CartSlice';
 import Profileslice from '../Slices/ProfileSlice';
+import CourseSlice from '../Slices/CourseSlice';
 
 const Rootreducer= combineReducers(
     {
         Auth:AuthSlice,
         Profile:Profileslice,
         Cart:CartSlice,
+        Course:CourseSlice,
     }
 );
 export default Rootreducer
