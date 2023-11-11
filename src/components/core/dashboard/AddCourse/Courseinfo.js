502132fd6cf6ae5aaa1edf64dff9428e179f4c1a
soni895 <darshansoni895@gmail.com>
2023-11-11 16:52:45 +0530

    code update

--- src/components/core/dashboard/AddCourse/Courseinfo.js
+++ src/components/core/dashboard/AddCourse/Courseinfo.js
@@ -1,8 +1,42 @@
 import React from 'react'
 
+import { useForm } from 'react-hook-form'
+import { useDispatch, useSelector } from 'react-redux';
 function Courseinfo() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors,isSubmitSuccessful },
+      } = useForm();
+
+      const dispatch=useDispatch();
+
+      const {Course,Editcourse}=useSelector(state=>state.Course);
+      const [laoding,setloading]=usestate(false);
+      const [Coursecategories,setCoursecategories]=usestate([]);
+      
+
+
+    
+      const Submithandler=(data)=>
+      {
+        console.log(data);
+      }
+
+    
   return (
-    <div>Courseinfo</div>
+    <div>
+
+  <form onSubmit={handleSubmit(Submithandler)}>
+      <input {...register('firstName')} />
+      <input {...register('lastName', { required: true })} />
+      {errors.lastName && <p>Last name is required.</p>}
+      <input {...register('age', { pattern: /\d+/ })} />
+      {errors.age && <p>Please enter number for age.</p>}
+      <input type="submit" />
+    </form>
+
+    </div>
   )
 }
 
