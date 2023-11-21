e7a5af9bc333cfb22654fbd10099f8b0107f0bc2
soni895 <darshansoni895@gmail.com>
2023-11-21 20:44:28 +0530

    code formatted

--- src/components/core/dashboard/AddCourse/Requirements.js
+++ src/components/core/dashboard/AddCourse/Requirements.js
@@ -1,100 +1,77 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-function Requirements({name, label, register, errors, setValue, getValues}) {
-  const [requirements,setrequirements]=useState('');
-  const [requirementslist,setrequirementslist]= useState([]);
+function Requirements({ name, label, register, errors, setValue, getValues }) {
+  const [requirements, setrequirements] = useState("");
+  const [requirementslist, setrequirementslist] = useState([]);
   console.log(requirementslist.length);
-  
 
-
-   useEffect(()=>
-   {
+  useEffect(() => {
     console.log("inside the first rendering");
-    register(name,{required:true,
-    ValidityState:(value)=>value.length>0})
-
-   },[]);
-useEffect(()=>
-{
+    register(name, {
+      required: true,
+      ValidityState: (value) => value.length > 0,
+    });
+  }, []);
+  useEffect(() => {
     console.log("inside the dependency list");
-      setValue(name,requirementslist);
-
-},[requirementslist]);
-const addhandler=()=>
-{
-    if(requirements)
-    {
-        console.log("requirements=>",requirements);
-        setrequirementslist([...requirementslist,requirements]);
-        setrequirements("");
-
+    setValue(name, requirementslist);
+  }, [requirementslist]);
+  const addhandler = () => {
+    if (requirements) {
+      console.log("requirements=>", requirements);
+      setrequirementslist([...requirementslist, requirements]);
+      setrequirements("");
     }
+  };
 
-}
-
-    const removehandler=(index)=>
-    {
-        const updatedList = [...requirementslist];
-        updatedList.splice(index, 1);
-        setrequirementslist(updatedList);
-    }
-    return (
+  const removehandler = (index) => {
+    const updatedList = [...requirementslist];
+    updatedList.splice(index, 1);
+    setrequirementslist(updatedList);
+  };
+  return (
     <div>
+      <label htmlFor={name}>{label}</label>
+      <input
+        className="text-black"
+        type="text"
+        id={name}
+        onChange={(e) => {
+          setrequirements(e.target.value);
+        }}
+      ></input>
+      <br></br>
+      <button
+        type="button"
+        onClick={addhandler}
+        className="font-semibold text-yellow-100"
+      >
+        add
+      </button>
 
-<label htmlFor={name}>{label}</label>
-<input
-className='text-black'
-type='text'
-id={name}
-onChange={(e)=>
-{
-    setrequirements(e.target.value);
-
-}}
-></input>
-<br></br>
-<button 
-type='button'
-onClick={addhandler}
-className='font-semibold text-yellow-100'>
-    add
-</button>
-
-{
-    
-    requirementslist.length>0 &&(
+      {requirementslist.length > 0 && (
         <ul>
-        {
-            requirementslist.map((requirement,index)=>
-            {
-               return  <li key={index}>
-                    
-                    <span>{requirement}</span>
-                    <button type='button'
-                    onClick={()=>
-                    {
-                        removehandler(index)
-                    }}
-                    className='font-semibold text-yellow-100'>clear</button>
-                    </li>
-            }
-            )
-
-        }
-           
+          {requirementslist.map((requirement, index) => {
+            return (
+              <li key={index}>
+                <span>{requirement}</span>
+                <button
+                  type="button"
+                  onClick={() => {
+                    removehandler(index);
+                  }}
+                  className="font-semibold text-yellow-100"
+                >
+                  clear
+                </button>
+              </li>
+            );
+          })}
         </ul>
-
-    )
-}
-{
-    errors[name] &&(
-        <span> is required</span>
-    )
- 
-}
-
+      )}
+      {errors[name] && <span> is required</span>}
     </div>
-  )
+  );
 }
 
-export default Requirements
+export default Requirements;
