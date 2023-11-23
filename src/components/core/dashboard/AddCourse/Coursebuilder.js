c7d1353ddd39c5a1a0b68ba72508cdc4e7da29ed
soni895 <darshansoni895@gmail.com>
2023-11-23 15:52:22 +0530

    formattted

--- src/components/core/dashboard/AddCourse/Coursebuilder.js
+++ src/components/core/dashboard/AddCourse/Coursebuilder.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
-import IconBtn from '../../../common/Iconbtn';
-import { setStep } from '../../../../Slices/CourseSlice';
-import { useState } from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import IconBtn from "../../../common/Iconbtn";
+import { setStep } from "../../../../Slices/CourseSlice";
+import { useState } from "react";
 import { IoIosAddCircleOutline } from "react-icons/io";
-import NestedView from './NestedView';
+import NestedView from "./NestedView";
 
 function Coursebuilder() {
-
-  const  [Editsectionname,setEditsectionname]= useState(null);
-const  {Course}= useSelector((state)=>state.Course);
+  const [Editsectionname, setEditsectionname] = useState(null);
+  const { Course } = useSelector((state) => state.Course);
   const {
     register,
     handleSubmit,
@@ -20,73 +19,59 @@ const  {Course}= useSelector((state)=>state.Course);
     formState: { errors },
   } = useForm();
 
-  const dispatch= useDispatch();
-  
-  const submithandler=(data)=>
-  {
+  const dispatch = useDispatch();
+
+  const submithandler = (data) => {
     console.log(data);
-    
-  }
-   
+  };
+
   return (
-
     <div>
-    <h1> Course Builder</h1>
+      <h1> Course Builder</h1>
 
-  <form onSubmit={handleSubmit(submithandler)}>
-    <div>
-    <label htmlFor='SectionName'> Course Builder<sup>*</sup></label>
-    <input
-    id='SectionName'
-    type='text'
-    placeholder='Enter Section Name'
-    {...register("SectionName",{required:true})}>
+      <form onSubmit={handleSubmit(submithandler)}>
+        <div>
+          <label htmlFor="SectionName">
+            {" "}
+            Course Builder<sup>*</sup>
+          </label>
+          <input
+            id="SectionName"
+            type="text"
+            placeholder="Enter Section Name"
+            {...register("SectionName", { required: true })}
+          ></input>
+          {errors.SectionName && <span> required</span>}
+          <div>
+            <IconBtn
+              text={Editsectionname ? "Edit Section Name" : "Create Section"}
+              outline={true}
+              customClasses={"text-white"}
+            >
+              <IoIosAddCircleOutline className="text-yellow-50 text-lg" />
+            </IconBtn>
+            {Editsectionname && (
+              <button
+                type="button"
+                onClick={() => (
+                  setEditsectionname(null), setValue("SectionName", "")
+                )}
+              >
+                {" "}
+                Cancel Course Name
+              </button>
+            )}
+          </div>
+        </div>
+      </form>
 
-    </input>
-    {
-      errors.SectionName && <span> required</span>
-    }
-    <div>
-      <IconBtn text={Editsectionname?"Edit Section Name":"Create Section"}
-      outline={true}
-      customClasses={"text-white"}
-      
-    >
-        <IoIosAddCircleOutline className='text-yellow-50 text-lg' />
-    </IconBtn>
-  {
-    Editsectionname && (
-    <button type='button'
-    onClick={()=>(
-      setEditsectionname(null),
-      setValue("SectionName","")
-    )
-    }> Cancel Course Name</button>
-    )
-  }
-
+      {Course.CourseContent.length > 0 && <NestedView />}
+      <div className="flex justify-end gap-10">
+        <IconBtn text="Back" onclick={() => dispatch(setStep(1))} />
+        <IconBtn text="Submit" onclick={() => dispatch(setStep(3))} />
+      </div>
     </div>
-    </div>
-    <div className='flex justify-end gap-10'>
-    <IconBtn 
-    text="Back"
-    onclick={()=>dispatch(setStep(1))}
-     />
-      <IconBtn 
-    text="Submit"
-    onclick={()=>dispatch(setStep(3))} />
-
-    </div>
-  
-  </form>
-
- { Course.CourseContent.length >0 &&(
-  <NestedView/>
- )
- }
- 
-    </div>
-  )
+  );
 }
 
-export default Coursebuilder
+export default Coursebuilder;
