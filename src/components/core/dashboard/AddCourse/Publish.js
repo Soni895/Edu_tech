7fde06c5f2bfb6c4a76d5d2d3fe9d9d30f2d3e00
soni895 <darshansoni895@gmail.com>
2023-11-24 16:56:21 +0530

    code updae

--- src/components/core/dashboard/AddCourse/Publish.js
+++ src/components/core/dashboard/AddCourse/Publish.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
 
 function Publish() {
-
   const {
     register,
     handleSubmit,
@@ -12,44 +11,32 @@ function Publish() {
     getValues,
     formState: { errors },
   } = useForm();
-  const dispatch= useDispatch();
-  const [loading,setloading]=useState();
-  const {Course}= useSelector((state)=>state.Course);
-  const {Token}= useSelector((state)=>state.Auth);
-  
+  const dispatch = useDispatch();
+  const [loading, setloading] = useState();
+  const { Course } = useSelector((state) => state.Course);
+  const { Token } = useSelector((state) => state.Auth);
 
-  const submithandler= (data)=>
-  {
+  const submithandler = (data) => {
     console.log(data);
-  }
-  
+  };
+
   return (
     <div>
-    <p> Publish Course</p>
-    <form onSubmit={handleSubmit(submithandler)}>
-
-<div>
-<label htmlFor='Public'>  make this Course As public</label>
-<input
-id='Public'
-type='checkbox'
-
-
- {...register("Public", { required: true })}></input>
- {
-  errors.Public && <span>please theck the box</span>
- }
-
-</div>
-<button>
-  submit
-</button>
-
-    </form>
-
-
+      <p> Publish Course</p>
+      <form onSubmit={handleSubmit(submithandler)}>
+        <div>
+          <label htmlFor="Public"> make this Course As public</label>
+          <input
+            id="Public"
+            type="checkbox"
+            {...register("Public", { required: true })}
+          ></input>
+          {errors.Public && <span>please theck the box</span>}
+        </div>
+        <button>submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Publish
+export default Publish;
