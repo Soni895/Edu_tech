0cdebfe5655af7b8943979d9f9cb2c7683bcf763
soni895 <darshansoni895@gmail.com>
2023-11-24 16:29:10 +0530

    item data imported

--- src/components/core/dashboard/AddCourse/Publish.js
+++ src/components/core/dashboard/AddCourse/Publish.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
 
 function Publish() {
 
@@ -11,6 +12,12 @@ function Publish() {
     getValues,
     formState: { errors },
   } = useForm();
+  const dispatch= useDispatch();
+  const [laoding,setlaoding]=useState();
+  const {Course}= useselector((state)=>state.Course);
+  const {Token}= useselector((state)=>state.Auth)
+
+  
   return (
     <div>Publish</div>
   )
