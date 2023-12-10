cc45b2de24701941ed78b73ca1d24c3fbc50a25e
soni895 <darshansoni895@gmail.com>
2023-12-10 14:36:12 +0530

    Error page update

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Error() {
   return (
-    <div className=' flex items-center justify-center text-3xl  text-white'>Error 404 not found</div>
+    <div className="flex flex-1 justify-center items-center text-white text-3xl">
+    Error 404 - Page Not Found
+  </div>
   )
 }
 
