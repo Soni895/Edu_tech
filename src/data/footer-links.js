34d4448b647687828ac84ac366cdb12807a1b311
soni895 <darshansoni895@gmail.com>
2024-01-10 12:12:15 +0530

    server and footer updated

--- src/data/footer-links.js
+++ src/data/footer-links.js
@@ -43,6 +43,7 @@ export const FooterLink2 = [
   {
     title: "Career building",
     links: [
+      
         {title: "Career paths", link: "/career-paths"},
         {title: "Career services", link: "/career-services"},
         {title: "Interview prep", link: "/interview-prep"},
